81a633da7a401e497342338157b968be02a5f334
Evan Prodromou <evan@prodromou.name>
2018-09-08 02:44:53 +0000

    Add a login form

--- login.js
+++ login.js
@@ -1,3 +1,3 @@
 const Login = Vue.extend({
-  template: '<div>Login</div>'
+  template: '<form><h2>Login</h2><label for="url">URL</label><input name="url" id="url">URL</input><label for="key">API Key</label><input name="key" id="key">Key</input><button>Login</button></form>'
 })
